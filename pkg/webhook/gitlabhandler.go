a4125472e83f36e21df2e123da9f7a0f1d397340
kairen <kyle.b@inwinstack.com>
2017-11-09 01:18:10 +0800

    Add multi build status

--- pkg/webhook/gitlabhandler.go
+++ pkg/webhook/gitlabhandler.go
@@ -11,20 +11,20 @@ import (
 // GitLabPipelineHandler handles gitlab pipeline events
 func GitLabPipelineHandler(payload interface{}, header webhooks.Header) {
 	pl := payload.(gitlab.PipelineEventPayload)
-	pepelineStatus := pl.ObjectAttributes.Status
 	id := strconv.FormatInt(pl.ObjectAttributes.ID, 10)
-	status := &api.GitHubStatus{
-		State:       pepelineStatus,
-		Context:     "gitlab-ci/pipeline",
-		Description: "Pipeline " + pepelineStatus,
-		TargetURL:   pl.Project.WebURL + "/pipelines/" + id,
+	for _, build := range pl.Builds {
+		stats := &api.GitHubStatus{
+			State:       build.Status,
+			Context:     build.Name,
+			Description: "Job " + build.Status,
+			TargetURL:   pl.Project.WebURL + "/pipelines/" + id,
+		}
+		stats.CheckStatus()
+		go api.CreateGitHubStatus(
+			pl.Project.Namespace,
+			pl.Project.Name,
+			pl.ObjectAttributes.SHA,
+			stats,
+		)
 	}
-	status.CheckStatus()
-
-	api.CreateGitHubStatus(
-		pl.Project.Namespace,
-		pl.Project.Name,
-		pl.ObjectAttributes.SHA,
-		status,
-	)
 }
